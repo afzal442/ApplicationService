a906c398c99e84f38c9c20208153113eb996ffbf
afzal442 <afzal442@gmail.com>
2022-07-01 00:24:56 +0530

    updates the link
    Signed-off-by: afzal442 <afzal442@gmail.com>

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -14,7 +14,7 @@ const FindUs = () => (
         <p className="p__opensans">Mon - Fri: 10:00 am - 08:00 pm</p>
         <p className="p__opensans">Sat - Sun: 10:00 am - 03:00 pm</p>
       </div>
-      <button type="button" className="custom__button" style={{ marginTop: '2rem' }} alt="/findUs">Visit Us</button>
+      <button type="button" className="custom__button" style={{ marginTop: '2rem' }}><a href="#contact">Visit Us</a></button>
     </div>
 
     <div className="app__wrapper_img">
